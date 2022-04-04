863f8576884184a87ebd810b53f75bb6b629d181
Boateng Dickson <89385798+dboatengg@users.noreply.github.com>
2022-04-04 12:24:07 +0000

    change path of logo

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const app = document.getElementById("root");
 const logo = document.createElement("img");
-logo.src = "/logo.png";
+logo.src = "logo.png";
 app.appendChild(logo);
 logo.className = "logo";
 
